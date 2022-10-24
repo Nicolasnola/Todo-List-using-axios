16540fa2fa28f585e0af5ebb5d174fccccc114e5
Nicolas Freitas <nicolas.freitasbr@gmail.com>
2022-10-24 20:29:17 -0300

    Adiciona componente de loading

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 import styles from "./styles";
 
 export default function Loading(){
     return(
         <View style={styles.loadView}>
-            <Text style={styles.loadingSt}>Loading</Text>
+           
+            <ActivityIndicator size={'large'}></ActivityIndicator>
         </View>
     )
 }
